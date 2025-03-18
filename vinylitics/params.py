f8306e22b3a2444c21d3137517a75c6c07f7b203
flore perrette <fmperrette@gmail.com>
2025-03-18 17:31:47 +0100

    adding to master

---
+++ vinylitics/params.py
@@ -0,0 +1,5 @@
+KEEP_COLUMNS = ['duration_ms', 'danceability', 'energy', 'key', 'loudness',
+       'mode', 'speechiness', 'acousticness', 'instrumentalness', 'liveness',
+       'valence', 'tempo', 'time_signature', 'popularity']
+
+#new line
