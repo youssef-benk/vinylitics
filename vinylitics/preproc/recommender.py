dd555f89aa9024cc9b1aad7ae418ae7131f52eef
Adviti Singh <adviti.s@gmail.com>
2025-03-19 14:13:32 +0100

    recommender v1

---
+++ vinylitics/preproc/recommender.py
@@ -0,0 +1,24 @@
+from vinylitics.preproc.data import load_and_clean_data
+from vinylitics.preproc.preprocessor import preprocess_features
+from vinylitics.preproc.model import neighbors_fit, find_neighbors
+
+df = load_and_clean_data()
+
+def recommend_track(track_name, artist):
+    track_name = track_name.lower().strip()
+    artist = artist.lower().strip()
+    try:
+        selected_track = df[(df['track_name'] == track_name) & (df['artists'] == artist)]
+    except:
+        print("Track not found")
+        return None
+
+    track_preproc = preprocess_features(selected_track)
+
+    pca, model = neighbors_fit(df)
+    neighbors = find_neighbors(track_preproc, model, pca)
+    print (neighbors)
+
+    return None
+
+recommend_track("Shape of You", "Ed Sheeran")
