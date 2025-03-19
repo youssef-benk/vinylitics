baf422dfefb524f1869787a01931601cba076c08
Suzie Beasse <beasse.suzie@gmail.com>
2025-03-19 13:29:27 +0000

    dill pickles for pca and neighbors

--- vinylitics/preproc/model.py
+++ vinylitics/preproc/model.py
@@ -1,6 +1,8 @@
 from sklearn.neighbors import NearestNeighbors
 import pandas as pd
 from sklearn.decomposition import PCA
+import dill
+from sklearn.pipeline import make_pipeline
 
 def neighbors_fit(
     X:pd.DataFrame,
@@ -16,12 +18,26 @@ def neighbors_fit(
         metrics (str, optional): _description_. Defaults to 'cosine'.
     """
     pca = PCA()
-    X_proj = pca.fit_transform(X)
-    nbrs = NearestNeighbors(n_neighbors=n_neighbors, algorithm=algorithm, metric=metrics).fit(X_proj)
+    pca.fit(X)
+    X_proj = pca.transform(X)
+    print("✅ Fitted PCA model")
 
-    return pca, nbrs
+        # Save transformer to a file using dill from your notebook
+    with open("pca.dill", "wb") as f:
+        dill.dump(pca, f)
+    print("✅ Saved fitted PCA to pca.dill")
 
-def find_neighbors(X_pred:pd.DataFrame, model, pca):
+    neighbors = NearestNeighbors(n_neighbors=n_neighbors, algorithm=algorithm, metric=metrics).fit(X_proj)
+    print("✅ Fitted NearestNeighbors model")
+
+    # Save transformer to a file using dill from your notebook
+    with open("neighbors.dill", "wb") as f:
+        dill.dump(neighbors, f)
+    print("✅ Saved NearestNeighbors model to neighbors_pipe.dill")
+
+    return None
+
+def find_neighbors(X_pred:pd.DataFrame):
     """Find the neighbors of a given song
 
     Args:
@@ -29,8 +45,17 @@ def find_neighbors(X_pred:pd.DataFrame, model, pca):
         model (str, optional): Nearest neighbors model.
         pca (PCA, optional): fitted pca
     """
-    X_pred_proj = pca.transform(X_pred)
+    # Load pca from the file using dill
+    with open("pca.dill", "rb") as f:
+        pca = dill.load(f)
 
-    distances, indices = model.kneighbors(X_pred_proj)
+    # Load model from the file using dill
+    with open("neighbors.dill", "rb") as f:
+        neighbors = dill.load(f)
 
+    X_proj = pca.transform(X_pred)
+    print("✅ Transformed X_pred with PCA")
+
+    distances, indices = neighbors.kneighbors(X_proj)
+    print("✅ Found neighbors")
     return distances, indices
