dee02f3647f5ff94533ba19dce228a2092133991
Adviti Singh <adviti.s@gmail.com>
2025-03-21 12:15:15 +0100

    name main

--- vinylitics/preproc/recommender.py
+++ vinylitics/preproc/recommender.py
@@ -47,7 +47,8 @@ def recommend_track(track_name, artist):
     distances, indices = find_neighbors(track_preproc)
 
     result = df.iloc[indices[0][1:]].sort_values(by='popularity', ascending=True)
-    print(result)
+    if __name__ == '__main__':
+        print(result)
     return result
-
-recommend_track("wut du u meen?", "Justen Beberr")
+if __name__ == '__main__':
+    recommend_track("wut du u meen?", "Justen Beberr")
